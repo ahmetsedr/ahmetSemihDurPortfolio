3c842e48c003f6963af6735a1c35fffe70c988bd
Ahmetsedr <ahmetsedur@gmail.com>
2023-09-17 12:58:43 +0300

    güncelleme 1.3.2

--- app/components/AboutCard.jsx
+++ app/components/AboutCard.jsx
@@ -16,7 +16,7 @@ export default function AboutCard() {
         <div className="w-full md:w-1/2 p-6 order-1 md:order-2">
             <h2 className="text-2xl font-semibold mb-4">Merhaba 👋, Ben Ahmet Semih</h2>
             <p className="text-sm md:text-base">
-                Üretken Akademi'nin Yoğun Yazılımcı Fabrikası Programı mezunu ve Emlak Yönetim A.Ş. stajyerliği yaptım. HTML, CSS, JavaScript ile birlikte React ve Next.js gibi teknolojilere hakimim. Figma ve GitHub gibi teknolojilere çalışabilirim. Detaylara önem verir, projeleri yaratıcı ve işlevsel bir şekilde hayata geçiririm. Yeni projelerde birlikte çalışmak için sabırsızlanıyorum.
+                Üretken Akademi nin Yoğun Yazılımcı Fabrikası Programı mezunu ve Emlak Yönetim A.Ş. stajyerliği yaptım. HTML, CSS, JavaScript ile birlikte React ve Next.js gibi teknolojilere hakimim. Figma ve GitHub gibi teknolojilere çalışabilirim. Detaylara önem verir, projeleri yaratıcı ve işlevsel bir şekilde hayata geçiririm. Yeni projelerde birlikte çalışmak için sabırsızlanıyorum.
             </p>
             <br />
             <Link href="/about" className='bg-white text-black border-black border-2 py-2 px-4 rounded-lg hover:bg-black hover:text-white hover:border-white transition duration-300'>
